c1806e0de8d07abb2e34215e915661ff6ff8b078
RubyDawes <ruebena.dawes@ndm.ox.ac.uk>
2025-04-24 14:03:54 +0100

    Add wrong ref annotation files to spliceai-splint tool
    Additional changes:
    - throw error if VCF doesn't indicate GrCH38
    - remove option to only annotate with subset of annotation files- I couldn't work out the logic! and I don't think it's advisable anyway.

--- tool/spliceAI-splint.sh
+++ tool/spliceAI-splint.sh
@@ -8,12 +8,11 @@ usage() {
     echo "Usage: $0 -i <input.vcf> [-o <output.vcf>] --anno_files <annotation1,annotation2,...>"
     echo "  -i, --input    Input VCF file"
     echo "  -o, --output   Output VCF file (default: <input_vcf>_spliceai_supplement.vcf.gz)"
-    echo "  --anno_files   Comma-separated list of annotation types to use (options: large_indel, annotation_change, distance, or 'all')"
     exit 1
 }
 
 # Parse command-line arguments
-ANNOTATIONS=("large_indel" "annotation_change" "distance")  # Default to 'all' if no argument is given
+ANNOTATIONS=("large_indel" "annotation_change" "distance", "wrong_ref")  # Default to 'all' if no argument is given
 while [[ $# -gt 0 ]]; do
     case "$1" in
         -i|--input)
@@ -22,12 +21,6 @@ while [[ $# -gt 0 ]]; do
             ;;
         -o|--output)
             FINAL_OUTPUT=$2
-            shift 2
-            ;;
-        --anno_files)
-            if [[ -n "$2" && "$2" != "all" ]]; then
-                IFS=',' read -ra ANNOTATIONS <<< "$2"
-            fi
             shift 2
             ;;
         *)
@@ -40,6 +33,12 @@ done
 if [[ -z "$VCF_FILE" ]]; then
     echo "Error: Input VCF file is required."
     usage
+fi
+
+# Check that the VCF header indicates GRCh38
+if ! zgrep -q "^##reference=GRCh38" "$VCF_FILE"; then
+    echo "Error: Input VCF does not declare reference genome as GRCh38 in the header. SpliceAI-splint can only be run on GRCh38 VCFs"
+    exit 1
 fi
 
 # Check if the input file is bgzipped, if not, compress it
@@ -55,7 +54,7 @@ elif [[ ! "$VCF_FILE" =~ \.vcf\.gz$ ]]; then
 fi
 
 # Set default output if not specified
-FINAL_OUTPUT=${FINAL_OUTPUT:-"${VCF_FILE/.vcf.gz/}.spliceai_supplement.vcf.gz"}
+FINAL_OUTPUT=${FINAL_OUTPUT:-"${VCF_FILE/.vcf.gz/}.spliceai_splint.vcf.gz"}
 
 # Check the chromosome naming style in the VCF file
 echo "Checking chromosome naming style in the input VCF..."
@@ -68,11 +67,13 @@ elif [[ "$FIRST_CHROM" =~ ^chr ]]; then
     INDEL_ANNOTATION_FILE="tool_annotation_files/indel_annotation_chr.sorted.txt.gz"
     SMALLVAR_ANNOTATION_FILE="tool_annotation_files/precomp_supplement_annotation_chr.sorted.txt.gz"
     DISTANCE_ANNOTATION_FILE="tool_annotation_files/distance_annotation_chr.sorted.txt.gz"
+    WRONGREF_ANNOTATION_FILE="tool_annotation_files/wrongref_annotation_chr.sorted.txt.gz"
 else
     echo "Detected non-'chr'-style chromosomes."
     INDEL_ANNOTATION_FILE="tool_annotation_files/indel_annotation_nochr.sorted.txt.gz"
     SMALLVAR_ANNOTATION_FILE="tool_annotation_files/precomp_supplement_annotation_nochr.sorted.txt.gz"
     DISTANCE_ANNOTATION_FILE="tool_annotation_files/distance_annotation_nochr.sorted.txt.gz"
+    WRONGREF_ANNOTATION_FILE="tool_annotation_files/wrongref_annotation_nochr.sorted.txt.gz"
 fi
 
 HEADER_FILE="${SMALLVAR_ANNOTATION_FILE/_chr.sorted.txt.gz/.hdr}"
@@ -82,63 +83,62 @@ HEADER_FILE="${HEADER_FILE/_nochr.sorted.txt.gz/.hdr}"
 OUTPUT_LARGE_INDEL="large_indel_annotated.vcf.gz"
 OUTPUT_SMALL_VARS="small_vars_annotated.vcf.gz"
 OUTPUT_DISTANCE="distance_annotated.vcf.gz"
+OUTPUT_WRONGREF="wrong_ref_annotated.vcf.gz"
 
-# Annotation steps based on user selection
-ANNOTATED_FILES=()
-if [[ " ${ANNOTATIONS[@]} " =~ " large_indel " ]]; then
-    if [[ ! -f "$INDEL_ANNOTATION_FILE" ]]; then
-        echo "Error: Annotation file '$INDEL_ANNOTATION_FILE' not found!"
-        exit 1
-    fi
-    echo "Annotating large indels..."
-    bcftools filter -i '(ILEN < -4 | ILEN > 1)' $VCF_FILE | \
-        bcftools annotate -a $INDEL_ANNOTATION_FILE -h $HEADER_FILE \
-        -c CHROM,FROM,TO,SPLICEAI_RECOMMENDATION,REASON \
-        -Oz -o "$OUTPUT_LARGE_INDEL"
-    tabix -p vcf "$OUTPUT_LARGE_INDEL"
-    ANNOTATED_FILES+=("$OUTPUT_LARGE_INDEL")
+# add indel 
+if [[ ! -f "$INDEL_ANNOTATION_FILE" ]]; then
+    echo "Error: Annotation file '$INDEL_ANNOTATION_FILE' not found!"
+    exit 1
 fi
+echo "Annotating large indels..."
+bcftools filter -i '(ILEN < -4 | ILEN > 1)' $VCF_FILE | \
+    bcftools annotate -a $INDEL_ANNOTATION_FILE -h $HEADER_FILE \
+    -c CHROM,FROM,TO,SPLICEAI_RECOMMENDATION,REASON \
+    -Oz -o "$OUTPUT_LARGE_INDEL"
+tabix -p vcf "$OUTPUT_LARGE_INDEL"
+ANNOTATED_FILES+=("$OUTPUT_LARGE_INDEL")
 
-if [[ " ${ANNOTATIONS[@]} " =~ " annotation_change " ]]; then
-    if [[ ! -f "$SMALLVAR_ANNOTATION_FILE" ]]; then
-        echo "Error: Annotation file '$SMALLVAR_ANNOTATION_FILE' not found!"
-        exit 1
-    fi
-    echo "Annotating remaining variants with precomputed annotations..."
-    bcftools filter -e '(ILEN < -4 | ILEN > 1)' "$VCF_FILE" | \
-    bcftools annotate --force -a "$SMALLVAR_ANNOTATION_FILE" -h "$HEADER_FILE" \
-        -c CHROM,FROM,TO,SPLICEAI_RECOMMENDATION,REASON \
-        -Oz -o "$OUTPUT_SMALL_VARS"
-    tabix -p vcf "$OUTPUT_SMALL_VARS"
-    
-    # Only add to ANNOTATED_FILES if distance annotation is not being applied later
-    if [[ ! " ${ANNOTATIONS[@]} " =~ " distance " ]]; then
-        ANNOTATED_FILES+=("$OUTPUT_SMALL_VARS")
-    fi
+# add transcript changes
+if [[ ! -f "$SMALLVAR_ANNOTATION_FILE" ]]; then
+    echo "Error: Annotation file '$SMALLVAR_ANNOTATION_FILE' not found!"
+    exit 1
 fi
+echo "Annotating remaining variants if in transcript change regions..."
+bcftools filter -e '(ILEN < -4 | ILEN > 1)' "$VCF_FILE" | \
+bcftools annotate --force -a "$SMALLVAR_ANNOTATION_FILE" -h "$HEADER_FILE" \
+    -c CHROM,FROM,TO,SPLICEAI_RECOMMENDATION,REASON \
+    -Oz -o "$OUTPUT_SMALL_VARS"
+tabix -p vcf "$OUTPUT_SMALL_VARS"
 
-if [[ " ${ANNOTATIONS[@]} " =~ " distance " ]]; then
-    if [[ ! -f "$DISTANCE_ANNOTATION_FILE" ]]; then
-        echo "Error: Annotation file '$DISTANCE_ANNOTATION_FILE' not found!"
-        exit 1
-    fi
-    echo "Annotating with distance annotations..."
-    
-    # Use OUTPUT_SMALL_VARS if annotation_change was applied, otherwise use VCF_FILE
-    INPUT_FILE="$OUTPUT_SMALL_VARS"
-    if [[ -z "$INPUT_FILE" ]]; then
-        INPUT_FILE="$VCF_FILE"
-    fi
-    
-    bcftools annotate --force -a "$DISTANCE_ANNOTATION_FILE" -h "$HEADER_FILE" \
-        -c CHROM,FROM,TO,SPLICEAI_RECOMMENDATION,REASON \
-        -Oz -o "$OUTPUT_DISTANCE" "$INPUT_FILE"
-    tabix -p vcf "$OUTPUT_DISTANCE"
-    ANNOTATED_FILES+=("$OUTPUT_DISTANCE")  # Only store the final file
+# add distance
+if [[ ! -f "$DISTANCE_ANNOTATION_FILE" ]]; then
+    echo "Error: Annotation file '$DISTANCE_ANNOTATION_FILE' not found!"
+    exit 1
 fi
+echo "Annotating variants if we recommend running with increased distance parameter..."
+
+bcftools annotate --force -a "$DISTANCE_ANNOTATION_FILE" -h "$HEADER_FILE" \
+    -c CHROM,FROM,TO,SPLICEAI_RECOMMENDATION,REASON \
+    -Oz -o "$OUTPUT_DISTANCE" "$OUTPUT_SMALL_VARS"
+tabix -p vcf "$OUTPUT_DISTANCE"
+
+if [[ ! -f "$WRONGREF_ANNOTATION_FILE" ]]; then
+    echo "Error: Annotation file '$WRONGREF_ANNOTATION_FILE' not found!"
+    exit 1
+fi
+echo "Annotating variants with wrong reference allele..."
+
+
+bcftools annotate --force -a "$WRONGREF_ANNOTATION_FILE" -h "$HEADER_FILE"  \
+    -c CHROM,FROM,TO,SPLICEAI_RECOMMENDATION,REASON \
+    -Oz -o "$OUTPUT_WRONGREF" "$OUTPUT_DISTANCE"
+tabix -p vcf "$OUTPUT_WRONGREF"
+
+ANNOTATED_FILES+=("$OUTPUT_WRONGREF")
 
 # Combine and sort the final output VCF
 echo "Creating and indexing the final annotated VCF..."
+echo $ANNOTATED_FILES
 bcftools concat -a -O z ${ANNOTATED_FILES[@]} | \
     bcftools sort -Oz -o $FINAL_OUTPUT
 
